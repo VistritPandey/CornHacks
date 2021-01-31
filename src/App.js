23782d2790bc480dbb37f5a6e9da1a52a31f5871
vistritpandey <vistritpandey@gmail.com>
2021-01-31 11:15:40 +0530

    Backframe done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import Feed from "./Feed";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
+import { login, logout } from "./features/userSlice";
+import { auth } from "./Firebase";
 
 function App() {
-  return <div className="App"></div>;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            id: authUser.id,
+            photoUrl: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="App">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
