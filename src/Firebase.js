ad23f069e2ad1b8586c7d58fe0a8cc207a3b0d64
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-02-01 08:10:35 -0500

    Config settings removed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5V0LJ3dwfMWHHFY51v1f6RoyMLxvxOfk",
-  authDomain: "creo-social.firebaseapp.com",
-  projectId: "creo-social",
-  storageBucket: "creo-social.appspot.com",
-  messagingSenderId: "685125851359",
-  appId: "1:685125851359:web:3663f2f609a7a42a31b16a",
-  measurementId: "G-JHY4BGZN13",
+  //add your own config file
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
